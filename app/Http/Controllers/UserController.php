d784caacd3c80d3dd8fa8e36d31429060a99df69
guilhermerrezende <guilhermerezdev@gmail.com>
2024-08-30 13:12:18 -0300

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserCreateRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+/**
+ * Class UserController
+ *
+ * @package App\Http\Controllers
+ * @author Vinícius Sarmento
+ * @link https://github.com/ViniciusSCS
+ * @date 2024-08-23 21:48:54
+ * @copyright UniEVANGÉLICA
+ */
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $user = User::select('id', 'name', 'email')->paginate('2');
+
+        return [
+            'status' => 200,
+            'menssagem' => 'Usuários encontrados!!',
+            'user' => $user
+        ];
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UserCreateRequest $request)
+    {
+        $data = $request->all();
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        return [
+            'status' => 200,
+            'menssagem' => 'Usuário cadastrado com sucesso!!',
+            'user' => $user
+        ];
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+
+
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email,' . $id,
+            'password' => 'nullable|string|min:8|confirmed',
+        ]);
+
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+
+        if ($request->has('password')) {
+            $user->password = bcrypt($request->input('password'));
+        }
+
+        $user->save();
+
+        return response()->json(['message' => 'Usuario UNI cadastrado com sucesso', 'user' => $user], 200);
+    }
+
+
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return response()->json(['message' => 'Usuario UNI deletado com sucesso'], 200);
+    }
+}
+
